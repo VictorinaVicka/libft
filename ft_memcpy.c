e39be67ee3b4e1e5f85071b8fb4dce0f5920f40f
VictorinaVicka <city_br@mail.ru>
2020-05-11 19:45:09 +0300

    сравнение строк не включая конец строки

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tfarenga <tfarenga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 15:07:08 by tfarenga          #+#    #+#             */
-/*   Updated: 2020/05/10 16:20:34 by tfarenga         ###   ########.fr       */
+/*   Updated: 2020/05/11 19:42:49 by tfarenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ void	*ft_memcpy(void *destination, const void *source, size_t n)
 
 	dst = (unsigned char *)destination;
 	src = (unsigned char *)source;
-	if (!destination || !source)
+	if (!dst && !src)
 		return (NULL);
-	while (n--)
+	while (n > 0)
 	{
 		*dst = *src;
 		dst++;
 		src++;
+		n--;
 	}
 	return (destination);
 }

b9df7d289bdb099d035e21ebf09e19e84b50c0ed
VictorinaVicka <city_br@mail.ru>
2020-05-11 18:28:12 +0300

    list/bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfarenga <tfarenga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 18:04:00 by tfarenga          #+#    #+#             */
+/*   Updated: 2020/05/11 18:08:40 by tfarenga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}

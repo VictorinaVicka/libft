1dbacba64edd567754153d7903bea838ad293df2
VictorinaVicka <city_br@mail.ru>
2020-05-11 20:07:06 +0300

    ищет заданный символ

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tfarenga <tfarenga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 19:15:23 by tfarenga          #+#    #+#             */
-/*   Updated: 2020/05/10 19:23:38 by tfarenga         ###   ########.fr       */
+/*   Updated: 2020/05/11 20:05:51 by tfarenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	*ft_memchr(const void *arr, int c, size_t n)
 {
-	unsigned char	*src;
-	size_t			index;
+	const char	*src;
 
-	index = 0;
-	src = (unsigned char *)arr;
-	while (src[index] != (unsigned char)c && index < n)
-		index++;
-	if (index == n)
-		return (NULL);
-	return ((void *)&src[index]);
+	src = arr;
+	while (n > 0)
+	{
+		if (*src == (char)c)
+			return ((void *)src);
+		src++;
+		n--;
+	}
+	return (NULL);
 }
